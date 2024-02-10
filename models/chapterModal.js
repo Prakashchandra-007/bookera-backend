0f184317ffc7c1fe6f1e52668fbb7dd3437e5f96
prakash chandra muduli <prakashchandramuduli007@gmail.com>
2024-02-10 23:08:23 +0530

    chapter creation and updation updated

--- models/chapterModal.js
+++ models/chapterModal.js
@@ -1,5 +1,36 @@
 const mongoose = require("mongoose");
-const chapterData = new mongoose.Schema({
+
+const chapterBlockSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Object,
+    required: true,
+  },
+});
+
+const chapterDataSchema = new mongoose.Schema({
+  time: {
+    type: Number,
+    required: true,
+  },
+  blocks: {
+    type: [chapterBlockSchema],
+    required: true,
+  },
+  version: {
+    type: String,
+    required: true,
+  },
+});
+
+const chapterSchema = new mongoose.Schema({
   book_id: {
     required: true,
     type: mongoose.Schema.ObjectId,
@@ -10,10 +41,6 @@ const chapterData = new mongoose.Schema({
     default: "",
   },
   name: {
-    type: String,
-    default: "",
-  },
-  html_content_text: {
     type: String,
     default: "",
   },
@@ -36,6 +63,7 @@ const chapterData = new mongoose.Schema({
   created_by: { type: String, default: null },
   updated_by: { type: String, default: null },
   restricted: { type: Array, default: [] },
+  chapterData: chapterDataSchema,
 });
 
-module.exports = mongoose.model("chapterData", chapterData);
+module.exports = mongoose.model("chapterData", chapterSchema);
